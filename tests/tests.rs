4dbb9b9e07da493c74fe9299fa8fb7d5b5589151
Elf M. Sternberg <elf.sternberg@gmail.com>
2019-08-27 16:55:10 -0700

    Command line tests have been added.

--- tests/tests.rs
+++ tests/tests.rs
@@ -22,12 +22,15 @@ fn cli_no_args() {
 }
 
 #[test]
-fn cli_small_board() {
+fn cli_small_board_custom_dict() {
     let mut board = NamedTempFile::new().unwrap();
-    write!(board, "an\utd\n").unwrap();
+    let mut dict = NamedTempFile::new().unwrap();
+    write!(board, "an\nut\n").unwrap();
+    write!(dict, "art\nant\nnut\naunt\ntan\n").unwrap();
+    let args = [board.path().to_str().unwrap(), "--dict", dict.path().to_str().unwrap()];
     Command::cargo_bin("boggle-solve")
         .unwrap()
-        .args(&[board.path()])
+        .args(&args)
         .assert()
         .stdout(contains("aunt"));
 }
